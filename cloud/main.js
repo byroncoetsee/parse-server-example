d06acc757944b77c9d024602392c2a9b80a45e2f
Byron Coetsee <byron.coetsee@afrozaar.com>
2017-05-28 17:07:45 +0200

    Cloud code fix?

--- cloud/main.js
+++ cloud/main.js
@@ -1,35 +1,4 @@
-// Panic-Dev
-
-var panicHandler = require('cloud/PanicHandler.js');
-
 
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
-});
-
-
-Parse.Cloud.job("cleanPanics", function(request, response) {
-    var query = new Parse.Query("Panics");
-    var d = new Date();
- 
-    query.equalTo("active", true);
-    query.find({
-        success: function(results) {
-            for (var i = 0; i < results.length; i++) {
-                if (results[i].updatedAt < (d.getTime() - (60 * 1000))) {
-                results[i].set("active",false);// = true;
-            }
-          }
- 
-          Parse.Object.saveAll(results,{
-            success: function(list) {
-                  // All the objects were saved.
-                  response.success("ok - updated: " + results.length);  //saveAll is now finished and we can properly exit with confidence :-)
-                    }, error: function(error) {
-                    // An error occurred while saving one of the objects.
-                    response.error("failure on saving list ");
-                  },});
-          // response.success("sweet");
-        }
-    })
 });
