193b0f044e98ec293d999002a3e793454a976fca
Byron Coetsee <byroncoetsee@gmail.com>
2017-05-29 10:58:42 +0200

    Update main.js
    Adding push fuction

--- cloud/main.js
+++ cloud/main.js
@@ -3,6 +3,41 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
 
+Parse.Cloud.define('push', function(req, res) {
+  var http = require("https");
+
+  var options = {
+    "method": "POST",
+    "hostname": "fcm.googleapis.com",
+    "port": null,
+    "path": "/fcm/send",
+    "headers": {
+      "authorization": "key=AAAAiT43N9A:APA91bE-DrOG3GhiwvvzJGdlEBpFgpwHomp51n7ZNo8Bx-T4yHrdSIiCbE4MHkEHruC_jzcQ6tsYRfVS4jWYuSdd9_F6uU1_3jreYpmazsPXao7a0RjqO-UeWMa8StZeyxV1MuPVfpeX",
+      "content-type": "application/json",
+      "cache-control": "no-cache",
+      "postman-token": "e79e9594-11a4-52b5-eb59-8bccb38943ad"
+    }
+  };
+
+  var req = http.request(options, function (res) {
+    var chunks = [];
+
+    res.on("data", function (chunk) {
+      chunks.push(chunk);
+    });
+
+    res.on("end", function () {
+      var body = Buffer.concat(chunks);
+      console.log(body.toString());
+    });
+  });
+
+  req.write(JSON.stringify({ notification: { title: 'Portugal vs. Denmark', body: '5 to 1' },
+    to: 'eZPuUJ8kJIk:APA91bHsDJ8Hss0xjWrdMplhDKWrfZoNyzJICWLAPgYvBbnArCN9r2ARiche7TYE1-2DhE5xSk343SDnpc4skzaHvAKr-SBbWPWOpc3UhH1Z9M_jN5MeCV4jOh7xiNGd6UTtxvNrAsRQ' }));
+  req.end();
+  res.success('Sent');
+}
+
 Parse.Cloud.job("cleanPanics", function(request, response) {
     var query = new Parse.Query("Panics");
     var d = new Date();
