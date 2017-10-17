869df4e11ab1bee1b7dc15ceff9d348ca5658b39
wprenison <wprenison@gmail.com>
2017-10-17 11:12:50 +0200

    - New alert hook to populate PanicGroup table - get active panics for a list of groups

--- cloud/main.js
+++ cloud/main.js
@@ -2,86 +2,75 @@
 var request;
 var response;
 
-Parse.Cloud.define("newAlertHook", function(req, resp) {
+Parse.Cloud.define("newAlertHook", function(req, resp) 
+{
 
   request = req
   response = resp
 
   var panic = request.params.panic;
-  var group = request.params.group;
+  var groups = request.params.groups;
   var user = request.params.user;
 
-  var object = new Parse.Object("PanicGroup");
-  object.set("panic", panic);
-  object.set("group", group);
-  object.set("user", user);
-
-  finished("ysy");
-
-  object.save(null, {
-    success: function(object) {
-      response.success("Updated: " + object);
-    },
-    error: function(object, error) {
-      response.error("Failure on saving objects");
-    }
-  });
+  // var createdPanicGroups = null;
+  //Create a Panic group record for each group
+  for(var groupIndex = 0; groupIndex < groups.length; groupIndex++)
+  {
+    var object = new Parse.Object("PanicGroup");
+    object.set("panic", panic);
+    object.set("group", groups[groupIndex]);
+    object.set("user", user);
+    object.save(null, 
+    {
+      success: function(object) 
+      {
+        if(groupIndex == groups.length) 
+          response.success("Created: " + object.id);
+        else
+          createdPanicGroups += object.id + "\n";
+      },
+      error: function(object, error) 
+      {
+        response.error("Failure on saving objects");
+      }
+    });
+  }
 });
 
 
-Parse.Cloud.define("getActiveAlerts", function(req, resp) {
+Parse.Cloud.define("getActiveAlerts", function(req, resp)
+ {
 
   request = req
   response = resp
 
-  var query = new Parse.Query("PanicGroup");
+  var d = new Date();
+  var numberOfHoursAgo = 24;
+  var groups = request.params.groups;
 
-  query.equalTo('group', request.params.group);
-  query.notEqualTo('user', null);
+  //Query for each group provided
+  for(var groupIndex = 0; groupIndex < groups.length; groupIndex++)
+  {
+    var query = new Parse.Query("PanicGroup");
 
-  // query.include('user');
+    query.equalTo('group', groups[groupIndex]);
+    // query.lessThanOrEqualTo('updatedAt', new Date(d.getTime() - (60 * 60 * numberOfHoursAgo * 1000)));
+    query.include('panic');
 
-  query.find({
-    useMasterKey: true,
-    success: function(result) {
+    query.find(
+    {
+      useMasterKey: true,
+      success: function(results)
+      {
+        finished(results);
+      },
+      error: function()
+       {
+        response.error(error);
+      }
+    });
+  }
 
-      // finished(result);
-      finished("yyay");
-      // var panicObject = result[0]
-
-      // var user = getUser(panicObject)
-      // var groups = getGroups(user);
-      // var location = getLocation(panicObject);
-
-      // if (groups.length == 0) {
-      //   response.error('No groups');
-      // }
-
-      // var groupsCheckedCounter = 0;
-
-      // var allIDs = {};
-
-      // finished(groups[2].toLowerCase().replace(/\b\w/g, l => l.toUpperCase()).replace(/\s/g,''));
-
-      // for (var i = 0; i < groups.length; ++i) {
-      //   getInstallationIDs(installationId, groups[i], function(IDs) {
-      //     allIDs = Object.assign(allIDs, IDs);
-      //     groupsCheckedCounter++;
-
-      //     if (groupsCheckedCounter == groups.length) {
-      //       finished(Object.keys(allIDs));
-      //       var keys = Object.keys(allIDs);
-
-      //       sendPush(keys, user, location, objectId);
-      //     }
-      //   });
-      // }
-
-    },
-    error: function() {
-      response.error(error);
-    }
-  });
 });
 
 
